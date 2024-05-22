1b5f3f457d2b8251a6e4aff19973ff52827f12bf
ALEXIS JAHIR LAPO CABRERA <alexis.lapo@epn.edu.ec>
2024-05-21 23:29:20 -0500

    Se mejoró el NavBar y se agregó el Footer

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import Traductor from './components/Traductor/Traductor'
+import Footer from './components/Footer/Footer'
 import './App.css'
 const App = () => {
   return (
     <div className='App'>
       <NavBar/>
       <Traductor/>
+      <Footer/>
     </div>
   )
 }
--- frontend/my-app/src/components/NavBar/NavBar.jsx
+++ frontend/my-app/src/components/NavBar/NavBar.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import './NavBar.css'
-import logo_empresa from '../../assets/logo.png'; 
+import logo_empresa from '../../assets/logo.png';
 const NavBar = () => {
     return (
         <div className='navbar'>
             <img src={logo_empresa} alt='Ironhack logo' className='logo' />
             <ul>
                 <li><a href='#'>Home</a></li>
-                <li><a href='#'>Productos</a></li>
-                <li><a href='#'>Features</a></li>
-                <li><a href='#'>Precios</a></li>
+                <li><a href='#'>PDF</a></li>
             </ul>
+            <div className='btn-modo'>
+                <div className="toggle">
+                    <input id="toggle-switch" type="checkbox"></input>
+                    <label for="toggle-switch"></label>
+                </div>
+            </div>
+
         </div>
     )
 }
