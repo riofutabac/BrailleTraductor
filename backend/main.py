27feab75fd8110de9e39b4a912f925e2a0bcf537
ALEXIS JAHIR LAPO CABRERA <alexis.lapo@epn.edu.ec>
2024-05-25 19:07:44 -0500

    Update host value in main.py for uvicorn.run

--- backend/main.py
+++ backend/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(routes.router, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
