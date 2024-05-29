97c8b7cf7760912bcd058791f6dabe5a442cf455
santiagobejarano <bejaranoj.santiago@gmail.com>
2024-05-29 15:45:51 -0500

    fix: Logica de Mayusculas Operativa
    Verificación de simbolos, mayusculas, y cadenas de mayúsculas

--- backend/app/services/translator.py
+++ backend/app/services/translator.py
@@ -22,16 +22,17 @@ secdict = {brailles[i]: codascii[i] for i in range(len(brailles))}
 def control_mayusculas_y_numeros(entry):
     modified = ""
     i = 0
+    last_was_space = True
     while i < len(entry):
         char = entry[i]
         if char.isdigit():
-            if i == 0 or not entry[i-1].isdigit():
+            if last_was_space or (i > 0 and entry[i-1] in ' ;:'):
                 # Iniciar una nueva secuencia numérica
                 modified += '⠼'
             modified += maindict[char]
         elif char.isupper():
-            if i == 0 or not entry[i-1].isupper() or entry[i-1] in ' .,;:':
-                # Iniciar una nueva secuencia de mayúsculas
+            if last_was_space:
+                # Comprobando si el siguiente caracter también es mayúscula
                 if i + 1 < len(entry) and entry[i+1].isupper() and entry[i+1] not in ' .,;:':
                     modified += '⠨⠨'  # Comienzo de palabras totalmente en mayúsculas
                 else:
@@ -39,6 +40,7 @@ def control_mayusculas_y_numeros(entry):
             modified += maindict[char.lower()]
         else:
             modified += maindict.get(char, char)  # Caracteres no alfabéticos o en minúsculas
+        last_was_space = char in ' .,;:'
         i += 1
     return modified
 
@@ -52,23 +54,23 @@ def tradEsp(entry):
     while i < len(entry):
         char = entry[i]
         if char == '⠨':
-            if i + 1 < len(entry) and entry[i+1] == '⠨':
-                i += 2
+            i += 1
+            if i < len(entry) and entry[i] == '⠨':
+                i += 1
                 while i < len(entry) and entry[i] not in ' .,;:':
                     if entry[i] in secdict:
                         texto += secdict[entry[i]].upper()
                     i += 1
-                i -= 1  # Compensar el incremento del ciclo principal
-            else:
-                i += 1
-                if i < len(entry) and entry[i] in secdict:
-                    texto += secdict[entry[i]].upper()
+                i -= 1
+            elif i < len(entry) and entry[i] in secdict:
+                texto += secdict[entry[i]].upper()
         elif char == '⠼':
+            # Asegurar que sólo se traduzcan números
             i += 1
-            while i < len(entry) and entry[i] in secdict and entry[i].isdigit():
+            while i < len(entry) and entry[i] in secdict and secdict[entry[i]].isdigit():
                 texto += secdict[entry[i]]
                 i += 1
-            i -= 1  # Compensar el incremento del ciclo principal
+            i -= 1
         elif char in secdict:
             texto += secdict[char]
         else:
