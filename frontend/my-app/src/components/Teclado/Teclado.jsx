539de8980cfff8afb3da991e97aa8610737b26d6
Flavio <matias.mejia@epn.edu.ec>
2024-05-31 07:53:58 -0500

    teclado con simbolos braille

--- frontend/my-app/src/components/Teclado/Teclado.jsx
+++ frontend/my-app/src/components/Teclado/Teclado.jsx
@@ -1,15 +1,27 @@
-
 import React from 'react';
 import './Teclado.css';
+
+// Diccionario de mapeo de caracteres ASCII a Braille
+const charToBrailleMap = {
+  'á': '⠷', 'é': '⠮', 'í': '⠌', 'ó': '⠬', 'ú': '⠾', 'ü': '⠳', '⭡_mayús': '⠨',
+  '¡!': '⠖', "'": '⠦', '#': '⠼', '+': '⠖', '-': '⠤', '*': '⠦', '÷': '⠲',
+  '"': '⠦', '(': '⠣', ')': '⠜', ':': '⠒', ';': '⠆', '.': '⠄', '=': '⠶', ',': '⠂', '¿?': '⠢', '@': '⠈',
+  'a': '⠁', 'b': '⠃', 'c': '⠉', 'd': '⠙', 'e': '⠑', 'f': '⠋', 'g': '⠛', 'h': '⠓', 'i': '⠊', 'j': '⠚',
+  'k': '⠅', 'l': '⠇', 'm': '⠍', 'n': '⠝', 'ñ': '⠻', 'o': '⠕', 'p': '⠏', 'q': '⠟', 'r': '⠗', 's': '⠎',
+  't': '⠞', 'u': '⠥', 'v': '⠧', 'w': '⠺', 'x': '⠭', 'y': '⠽', 'z': '⠵', '[': '⠣', ']': '⠜', '^': '⠘', '_': '⠤',
+  '0': '⠚', '1': '⠁', '2': '⠃', '3': '⠉', '4': '⠙', '5': '⠑', '6': '⠋', '7': '⠛', '8': '⠓', '9': '⠊'
+};
 
 export const Teclado = () => {
   const keys = [
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
-    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ'],
-    ['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace'],
-    ['Shift', 'Space', 'Enter'],
-    ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ñ'],
+    ['⭡_mayús','z', 'x', 'c', 'v', 'b', 'n', 'm', 'borrar'],
+    ['á', 'é', 'í', 'ó', 'ú', 'ü', 'enter'],
+    ['¡!', '@', '#', '^',  '*', '(', ')'],
+    ['"', "'", '+', '-', ':', ';', '.'], 
+    ['=', ',', '¿?', '_', 'espacio']
   ];
 
   return (
@@ -17,7 +29,10 @@ export const Teclado = () => {
       {keys.map((row, rowIndex) => (
         <div key={rowIndex} className="keyboard-row">
           {row.map((key) => (
-            <button key={key} className="key">{key}</button>
+            <button key={key} className="key">
+              <div className="braille-symbol">{charToBrailleMap[key.toLowerCase()] || key}</div>
+              <div className="ascii-symbol">{key}</div>
+            </button>
           ))}
         </div>
       ))}
